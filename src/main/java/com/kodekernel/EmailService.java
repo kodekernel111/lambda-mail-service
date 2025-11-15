ae78fc7bdf12a389df8a27b7163a7f64229b679c
Namra Gandhi <namragandhi20@gmail.com>
2025-11-15 15:02:38 +0530

    Revert "Add APIGateway Request and Response"
    This reverts commit 22211c4d4892871f7d90f8f9f1ec476c3e6a9ffb.

--- src/main/java/com/kodekernel/EmailService.java
+++ src/main/java/com/kodekernel/EmailService.java
@@ -2,15 +2,10 @@ package com.kodekernel;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.amazonaws.services.simpleemail.*;
 import com.amazonaws.services.simpleemail.model.*;
-import com.google.gson.Gson;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-public class EmailService implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+public class EmailService implements RequestHandler<EmailRequest, String> {
 
     private final String FROM = System.getenv("FROM_EMAIL");
     private final String TO = System.getenv("TO_EMAIL");
@@ -19,7 +14,7 @@ public class EmailService implements RequestHandler<APIGatewayV2HTTPEvent, APIGa
             : "ap-south-1";
 
     @Override
-    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+    public String handleRequest(EmailRequest input, Context context) {
 
         try {
             AmazonSimpleEmailService ses = AmazonSimpleEmailServiceClientBuilder
@@ -27,39 +22,24 @@ public class EmailService implements RequestHandler<APIGatewayV2HTTPEvent, APIGa
                     .withRegion(REGION)
                     .build();
 
-            String body = event.getBody();
-            if(body == null) {
-                throw new RuntimeException("Request body is null");
-            }
-
-            EmailRequest req = new Gson().fromJson(body, EmailRequest.class);
-            log.info("Received request: Name: {}, Email: {}, Service: {}, Message: {}",req.getName(), req.getEmail(), req.getService(), req.getMessage());
-
             SendEmailRequest request = new SendEmailRequest()
                     .withSource(FROM)
                     .withDestination(new Destination().withToAddresses(TO))
                     .withMessage(
                             new Message()
-                                    .withSubject(new Content("Contact Form: " + req.getService()))
+                                    .withSubject(new Content("Contact Form: " + input.getService()))
                                     .withBody(new Body().withText(new Content(
-                                            "Name: " + req.getName() + "\n" +
-                                                    "Email: " + req.getEmail() + "\n" +
-                                                    "Message: " + req.getMessage()
+                                            "Name: " + input.getName() + "\n" +
+                                                    "Email: " + input.getEmail() + "\n" +
+                                                    "Message: " + input.getMessage()
                                     )))
                     );
 
             ses.sendEmail(request);
 
-            return APIGatewayV2HTTPResponse.builder()
-                    .withStatusCode(200)
-                    .withBody("{\"message\": \"Email sent successfully!\"}")
-                    .build();
+            return "Email sent successfully!";
         } catch (Exception e) {
-            log.error(e.getMessage(), e);
-            return APIGatewayV2HTTPResponse.builder()
-                    .withStatusCode(500)
-                    .withBody("{\"message\": \"Failed to send email\"}")
-                    .build();
+            return "Failed to send email: " + e.getMessage();
         }
     }
 }
